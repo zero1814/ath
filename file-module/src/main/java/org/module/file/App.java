b492a9d3e1353fab070423c8b98db1ed5aea9dcc
zhy <zero1814@126.com>
2016-09-17 16:33:58 +0800

    文件相关操作从核心模块分离

---
+++ file-module/src/main/java/org/module/file/App.java
@@ -0,0 +1,13 @@
+package org.module.file;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ file-module/src/main/java/org/module/file/handler/ExcelHandler.java
@@ -0,0 +1,12 @@
+package org.module.file.handler;
+
+/**
+ * 
+ * 类: ExcelHandler <br>
+ * 描述: excel相关操作类 <br>
+ * 作者: zhy<br>
+ * 时间: 2016年9月17日 下午4:32:31
+ */
+public class ExcelHandler {
+
+}
