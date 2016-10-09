3910907bd2ea77089a79bced0f7ebe6a1ac4c44b
zhanghaiyu <zhanghy@huijiayou.cn>
2016-10-09 12:28:23 +0800

    测试redis是否能调用

--- commons-module/src/test/java/org/module/commons/RedisTest.java
+++ commons-module/src/test/java/org/module/commons/RedisTest.java
@@ -2,15 +2,27 @@ package org.module.commons;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:spring/spring.xml", "classpath:spring/spring.redis.xml" })
 public class RedisTest {
 
+	@Autowired
+	private JedisPool pool;
+
 	@Test
 	public void conn() {
-		System.out.println("------->");
+		Jedis jedis = pool.getResource();
+		if (jedis != null) {
+			jedis.set("address", "测试数据");
+		} else {
+			System.out.println("jedis为空");
+		}
 	}
 }
