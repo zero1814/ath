c1cadb67bcdc5ac0f8e0766d164a74625b1f3fc4
zhanghaiyu <zhanghy@huijiayou.cn>
2016-12-29 07:37:34 +0800

    zhy

--- commons-module/src/main/java/org/module/commons/helper/CodeHelper.java
+++ commons-module/src/main/java/org/module/commons/helper/CodeHelper.java
@@ -4,8 +4,34 @@ import org.module.commons.util.UniqueIdUtil;
 
 public class CodeHelper {
 
-	public static String getUniqueCode(String data, String prefix) {
+	/**
+	 * 
+	 * 方法: getUniqueCode <br>
+	 * 描述: 获取唯一编码 <br>
+	 * 作者: zhy<br>
+	 * 时间: 2016年12月29日 上午7:11:12
+	 * 
+	 * @param workerId
+	 * @param datacenterId
+	 * @param prefix
+	 * @return
+	 */
+	public static String getUniqueCode(long workerId, long datacenterId, String prefix) {
 		long workid = 1;
 		return prefix + UniqueIdUtil.getInstance(workid, workid).nextId();
+	}
+
+	/**
+	 * 
+	 * 方法: getUniqueCode <br>
+	 * 描述: 获取唯一编码 <br>
+	 * 作者: zhy<br>
+	 * 时间: 2016年12月29日 上午7:17:48
+	 * 
+	 * @param prefix
+	 * @return
+	 */
+	public static String getUniqueCode(String prefix) {
+		return prefix + UniqueIdUtil.getInstance();
 	}
 }
