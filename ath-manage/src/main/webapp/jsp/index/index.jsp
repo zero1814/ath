dc5e4dec16220263d0ab7a4e2fe0f4a75c200f16
zero1814 <zero1814@126.com>
2017-02-02 19:09:35 +0800

    zhy

---
+++ ath-manage/src/main/webapp/jsp/index/index.jsp
@@ -0,0 +1,7 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ include file="/jsp/commons/header.jsp"%>
+ <div class="row">
+	<h1>首页</h1>
+</div>
+<%@ include file="/jsp/commons/footer.jsp"%>
--- kernel-module/src/main/java/org/module/controller/kernel/CmDatabaseController.java
+++ kernel-module/src/main/java/org/module/controller/kernel/CmDatabaseController.java
@@ -9,6 +9,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+/**
+ * 
+ * 类: CmDatabaseController <br>
+ * 描述: 数据库相关页面交互 <br>
+ * 作者: zhy<br>
+ * 时间: 2017年2月2日 下午6:17:13
+ */
 @Controller
 @RequestMapping("kernel/db/")
 public class CmDatabaseController {
@@ -16,26 +23,87 @@ public class CmDatabaseController {
 	@Autowired
 	private ICmDatabaseService service;
 
+	/**
+	 * 
+	 * 方法: index <br>
+	 * 描述: 数据库列表显示页面 <br>
+	 * 作者: zhy<br>
+	 * 时间: 2017年2月2日 下午6:17:31
+	 * @return
+	 */
 	@RequestMapping("index")
 	public String index() {
 		return null;
 	}
 
+	/**
+	 * 
+	 * 方法: data <br>
+	 * 描述: 数据库列表数据获取 <br>
+	 * 作者: zhy<br>
+	 * 时间: 2017年2月2日 下午6:17:53
+	 * @param dto
+	 * @return
+	 */
 	@RequestMapping("data")
 	@ResponseBody
 	public DataResult data(CmDatabaseDto dto) {
 		return service.findDataAll(dto);
 	}
-	
-	@RequestMapping("db/tables")
+
+	/**
+	 * 
+	 * 方法: tableIndex <br>
+	 * 描述: 数据库所有表显示页面 <br>
+	 * 作者: zhy<br>
+	 * 时间: 2017年2月2日 下午6:18:10
+	 * @return
+	 */
+	@RequestMapping("table/index")
+	public String tableIndex() {
+		return null;
+	}
+
+	/**
+	 * 
+	 * 方法: dbTables <br>
+	 * 描述: 数据库所有表显示数据 <br>
+	 * 作者: zhy<br>
+	 * 时间: 2017年2月2日 下午6:18:28
+	 * @param dto
+	 * @return
+	 */
+	@RequestMapping("table/data")
 	@ResponseBody
-	public DataMapResult dbTables(CmDatabaseDto dto){
+	public DataMapResult dbTables(CmDatabaseDto dto) {
 		return service.findTableByDb(dto);
 	}
-	
-	@RequestMapping("db/table/columns")
+
+	/**
+	 * 
+	 * 方法: tableColumnsIndex <br>
+	 * 描述: 数据库所有表列字段显示页面 <br>
+	 * 作者: zhy<br>
+	 * 时间: 2017年2月2日 下午6:18:44
+	 * @return
+	 */
+	@RequestMapping("table/column/index")
+	public String tableColumnsIndex() {
+		return null;
+	}
+
+	/**
+	 * 
+	 * 方法: tableColumns <br>
+	 * 描述: 数据库所有表列字段显示数据 <br>
+	 * 作者: zhy<br>
+	 * 时间: 2017年2月2日 下午6:19:01
+	 * @param dto
+	 * @return
+	 */
+	@RequestMapping("table/column/data")
 	@ResponseBody
-	public DataMapResult tableColumns(CmDatabaseDto dto){
+	public DataMapResult tableColumns(CmDatabaseDto dto) {
 		return service.findColumnByTable(dto);
 	}
 }
