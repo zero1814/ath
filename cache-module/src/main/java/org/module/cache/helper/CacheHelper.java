9a305a5300bcfdbb9c1cb3aeb83c333cbf500f15
zhanghaiyu <zhanghy@huijiayou.cn>
2017-02-04 15:35:19 +0800

    添加缓存分支

---
+++ cache-module/src/main/java/org/module/cache/helper/CacheHelper.java
@@ -0,0 +1,5 @@
+package org.module.cache.helper;
+
+public class CacheHelper {
+
+}
---
+++ cache-module/src/main/java/org/module/cache/util/RedisCache.java
@@ -0,0 +1,22 @@
+package org.module.cache.util;
+
+import org.module.commons.base.BaseClass;
+import org.springframework.stereotype.Component;
+
+import com.google.inject.Inject;
+
+import redis.clients.jedis.Jedis;
+
+/**
+ * 
+ * 类: RootCache <br>
+ * 描述: redis缓存相关操作  <br>
+ * 作者: zhy<br>
+ * 时间: 2017年2月4日 上午9:27:02
+ */
+@Component
+public class RedisCache extends BaseClass {
+
+	@Inject
+	private Jedis jedis;
+}
