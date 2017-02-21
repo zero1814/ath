c383ba8be4f393728c64990dc6fe9947755a6321
zhanghaiyu <zhanghy@huijiayou.cn>
2017-02-21 15:07:50 +0800

    zhy

--- system-module/src/main/java/org/module/dto/system/role/SmRoleDto.java
+++ system-module/src/main/java/org/module/dto/system/role/SmRoleDto.java
@@ -3,8 +3,18 @@ package org.module.dto.system.role;
 import org.module.dto.BaseDto;
 
 public class SmRoleDto extends BaseDto {
+	
+	private String code;
 
 	private String name;
+	
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
 
 	public String getName() {
 		return name;
