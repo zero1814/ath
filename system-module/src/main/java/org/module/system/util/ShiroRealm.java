d0840100dc793cef5907f318d6336a0d366d4bdb
zhanghaiyu <zhanghy@huijiayou.cn>
2017-04-08 11:36:39 +0800

    zhy

---
+++ system-module/src/main/java/org/module/system/util/ShiroRealm.java
@@ -0,0 +1,22 @@
+package org.module.system.util;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+
+public class ShiroRealm extends AuthorizingRealm {
+
+	@Override
+	protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principal) {
+		return null;
+	}
+
+	@Override
+	protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+		return null;
+	}
+
+}
