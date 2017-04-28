320de88684b90d232e2d3ff4f3e1bf3dcad49533
zhanghaiyu <zhanghy@huijiayou.cn>
2017-04-28 16:36:57 +0800

    zhy

---
+++ system-module/src/main/java/org/module/model/system/user/SmUser.java
@@ -0,0 +1,5 @@
+package org.module.model.system.user;
+
+public class SmUser {
+
+}
--- system-module/src/main/java/org/module/shiro/UserRoleRealm.java
+++ system-module/src/main/java/org/module/shiro/UserRoleRealm.java
@@ -1,11 +1,18 @@
 package org.module.shiro;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
 import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
+import org.module.model.system.user.SmUser;
 
 public class UserRoleRealm extends AuthorizingRealm {
 
@@ -20,9 +27,21 @@ public class UserRoleRealm extends AuthorizingRealm {
 	 */
 	@Override
 	protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principal) {
-		AuthorizationInfo info = null;
-		
-		return info;
+		SimpleAuthorizationInfo authorizationInfo = new SimpleAuthorizationInfo();
+		// 用户名
+		// String username = (String) principal.getPrimaryPrincipal();
+		/**
+		 * 获取用户角色信息
+		 */
+		Set<String> roles = null;
+		authorizationInfo.setRoles(roles);
+		/**
+		 * 获取用户权限信息
+		 */
+		Set<String> permissions = null;
+		authorizationInfo.setStringPermissions(permissions);
+
+		return authorizationInfo;
 	}
 
 	/**
@@ -37,7 +56,9 @@ public class UserRoleRealm extends AuthorizingRealm {
 	 */
 	@Override
 	protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
-		AuthenticationInfo info = null;
+		SimpleAuthenticationInfo info = null;
+		String username = (String) token.getPrincipal();
+		SmUser user = null;
 		return info;
 	}
 
