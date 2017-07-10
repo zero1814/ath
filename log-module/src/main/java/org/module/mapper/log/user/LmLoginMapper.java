d7784621036d12012ca346db2019d85eb2da4e9a
zhanghaiyu <zhanghy@huijiayou.cn>
2017-07-10 17:23:14 +0800

    zhy

--- log-module/src/main/java/org/module/mapper/log/user/LmLoginMapper.java
+++
@@ -1,9 +0,0 @@
-package org.module.mapper.log.user;
-
-import org.module.dto.BaseDto;
-import org.module.mapper.BaseMapper;
-import org.module.model.log.user.LmLogin;
-
-public interface LmLoginMapper extends BaseMapper<LmLogin, BaseDto> {
-
-}
