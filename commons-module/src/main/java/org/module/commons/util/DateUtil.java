d6ee2e79354f9454810e62d539c2f011ac3fbae4
zhanghaiyu <zhanghaiyu@reignwood.com>
2017-10-16 11:25:41 +0800

    添加周相关计算

--- commons-module/src/main/java/org/module/commons/util/DateUtil.java
+++ commons-module/src/main/java/org/module/commons/util/DateUtil.java
@@ -2,7 +2,9 @@ package org.module.commons.util;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 /**
  * 
@@ -103,5 +105,120 @@ public class DateUtil {
 	 */
 	public static Date strToDateTime(String str) throws ParseException {
 		return sdfDateTime.parse(str);
+	}
+
+	/**
+	 * ================= 周计算及转换 =============
+	 */
+
+	/**
+	 * 
+	 * 方法: getWeekOfYear <br>
+	 * 描述: 获取当前时间所在年的周数 <br>
+	 * 作者: zhy<br>
+	 * 时间: 2017年10月16日 上午11:23:37
+	 * 
+	 * @param date
+	 * @return
+	 */
+	public static int getWeekOfYear(Date date) {
+		Calendar c = new GregorianCalendar();
+		c.setFirstDayOfWeek(Calendar.MONDAY);
+		c.setMinimalDaysInFirstWeek(7);
+		c.setTime(date);
+		return c.get(Calendar.WEEK_OF_YEAR);
+	}
+
+	/**
+	 * 
+	 * 方法: getMaxWeekNumOfYear <br>
+	 * 描述: 获取当前时间所在年的最大周数 <br>
+	 * 作者: zhy<br>
+	 * 时间: 2017年10月16日 上午11:23:59
+	 * 
+	 * @param year
+	 * @return
+	 */
+	public static int getMaxWeekNumOfYear(int year) {
+		Calendar c = new GregorianCalendar();
+		c.set(year, Calendar.DECEMBER, 31, 23, 59, 59);
+		return getWeekOfYear(c.getTime());
+	}
+
+	/**
+	 * 
+	 * 方法: getFirstDayOfWeek <br>
+	 * 描述: 获取某年的第几周的开始日期 <br>
+	 * 作者: zhy<br>
+	 * 时间: 2017年10月16日 上午11:24:19
+	 * 
+	 * @param year
+	 * @param week
+	 * @return
+	 */
+	public static Date getFirstDayOfWeek(int year, int week) {
+		Calendar c = new GregorianCalendar();
+		c.set(Calendar.YEAR, year);
+		c.set(Calendar.MONTH, Calendar.JANUARY);
+		c.set(Calendar.DATE, 1);
+		Calendar cal = (GregorianCalendar) c.clone();
+		cal.add(Calendar.DATE, week * 7);
+		return getFirstDayOfWeek(cal.getTime());
+	}
+
+	/**
+	 * 
+	 * 方法: getLastDayOfWeek <br>
+	 * 描述: 获取某年的第几周的结束日期 <br>
+	 * 作者: zhy<br>
+	 * 时间: 2017年10月16日 上午11:24:55
+	 * @param year
+	 * @param week
+	 * @return
+	 */
+	public static Date getLastDayOfWeek(int year, int week) {
+		Calendar c = new GregorianCalendar();
+		c.set(Calendar.YEAR, year);
+		c.set(Calendar.MONTH, Calendar.JANUARY);
+		c.set(Calendar.DATE, 1);
+		Calendar cal = (GregorianCalendar) c.clone();
+		cal.add(Calendar.DATE, week * 7);
+		return getLastDayOfWeek(cal.getTime());
+	}
+
+	/**
+	 * 
+	 * 方法: getFirstDayOfWeek <br>
+	 * 描述: 获取当前时间所在周的开始日期 <br>
+	 * 作者: zhy<br>
+	 * 时间: 2017年10月16日 上午11:24:47
+	 * 
+	 * @param date
+	 * @return
+	 */
+	public static Date getFirstDayOfWeek(Date date) {
+		Calendar c = new GregorianCalendar();
+		c.setFirstDayOfWeek(Calendar.MONDAY);
+		c.setTime(date);
+		c.set(Calendar.DAY_OF_WEEK, c.getFirstDayOfWeek()); // Monday
+		return c.getTime();
+	}
+
+	/**
+	 * 
+	 * 方法: getLastDayOfWeek <br>
+	 * 描述: 获取当前时间所在周的结束日期 <br>
+	 * 作者: zhy<br>
+	 * 时间: 2017年10月16日 上午11:24:41
+	 * 
+	 * @param date
+	 * @return
+	 */
+	public static Date getLastDayOfWeek(Date date) {
+		Calendar c = new GregorianCalendar();
+		c.setFirstDayOfWeek(Calendar.MONDAY);
+		c.setTime(date);
+		c.set(Calendar.DAY_OF_WEEK, c.getFirstDayOfWeek() + 6); // Sunday
+		return c.getTime();
 	}
 }
