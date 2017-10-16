0de8a9342277170e90e1a650440b0bc82b13d37a
zhanghaiyu <zhanghaiyu@reignwood.com>
2017-10-16 13:12:01 +0800

    重新设计开发框架

---
+++ service-module/src/main/java/org/module/result/PageResult.java
@@ -0,0 +1,39 @@
+package org.module.result;
+
+import java.util.List;
+
+import org.module.model.BaseModel;
+
+
+/**
+ * 
+ * 类: PageResult <br>
+ * 描述: 分页结果集 <br>
+ * 作者: zhy<br>
+ * 时间: 2016年10月24日 上午7:33:55
+ * 
+ * @param <T>
+ */
+public class PageResult extends BaseResult {
+
+	private List<? extends BaseModel> rows;
+
+	private Integer total;
+
+	public List<? extends BaseModel> getRows() {
+		return rows;
+	}
+
+	public void setRows(List<? extends BaseModel> rows) {
+		this.rows = rows;
+	}
+
+	public Integer getTotal() {
+		return total;
+	}
+
+	public void setTotal(Integer total) {
+		this.total = total;
+	}
+
+}
---
+++ service-module/src/main/java/org/module/result/UploadResult.java
@@ -0,0 +1,22 @@
+package org.module.result;
+
+/**
+ * 
+ * 类: UploadResult <br>
+ * 描述: 文件上传返回结果集 <br>
+ * 作者: zhy<br>
+ * 时间: 2017年6月23日 下午5:13:38
+ */
+public class UploadResult extends BaseResult {
+
+	private String path;
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+
+}
