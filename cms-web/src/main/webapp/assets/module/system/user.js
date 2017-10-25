38795e22134408c7ecba94ec263f787b3a4ba729
zhanghaiyu <zhanghaiyu@reignwood.com>
2017-10-25 16:25:13 +0800

    zhy

---
+++ cms-web/src/main/webapp/assets/module/system/user.js
@@ -0,0 +1,20 @@
+/**
+ * 系统用户相关函数
+ */
+var User = {
+	init : function() {
+
+	},
+	data : function() {
+	},
+	search : function() {
+	},
+	add : function() {
+	},
+	edit : function() {
+	},
+	del : function() {
+	},
+	sel : function() {
+	}
+};
