b5d57a4d30d3e7e50295e68dfb2a5caf04d9c9ff
zhanghaiyu <zhanghaiyu@reignwood.com>
2017-10-30 13:41:40 +0800

    定时模块

---
+++ quartz-module/src/main/java/org/module/quartz/listener/TriggerListener.java
@@ -0,0 +1,12 @@
+package org.module.quartz.listener;
+
+import org.quartz.listeners.TriggerListenerSupport;
+
+public class TriggerListener extends TriggerListenerSupport {
+
+	@Override
+	public String getName() {
+		return null;
+	}
+
+}
